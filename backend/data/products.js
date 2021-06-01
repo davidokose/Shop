19366deed01da76b542a7181fa80e9a782d13204
davidokose <david.okose@gmail.com>
2021-06-01 11:29:47 +0100

    made some changes

---
+++ backend/data/products.js
@@ -0,0 +1,112 @@
+const products = [
+  {
+    name: 'Airpods Wireless Bluetooth Headphones',
+    image: '/images/airpods.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 14,
+  },
+  {
+    name: 'iPhone 11 Pro 256GB Memory',
+    image: '/images/phone.jpg',
+    description:
+      'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 599.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    name: 'Cannon EOS 80D DSLR Camera',
+    image: '/images/camera.jpg',
+    description:
+      'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+    brand: 'Cannon',
+    category: 'Electronics',
+    price: 929.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 6,
+  },
+  {
+    name: 'Sony Playstation 4 Pro White Version',
+    image: '/images/playstation.jpg',
+    description:
+      'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
+    brand: 'Sony',
+    category: 'Electronics',
+    price: 399.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 8,
+  },
+  {
+    name: 'Logitech G-Series Gaming Mouse',
+    image: '/images/mouse.jpg',
+    description:
+      'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
+    brand: 'Logitech',
+    category: 'Electronics',
+    price: 49.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    name: 'Amazon Echo Dot 3rd Generation',
+    image: '/images/alexa.jpg',
+    description:
+      'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
+    brand: 'Amazon',
+    category: 'Electronics',
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 7,
+  },
+  {
+    name: 'Wahl Lithium Ion Pro Series Cordless Animal Clippers',
+    image: '/images/clipper.jpg',
+    description:
+      'Rechargeable, Quiet, Low Noise, Heavy-Duty, Electric Dog & Cat Grooming Kit for Small & Large Breeds with Thick to Heavy Coats – Model 9766,Black and Silver',
+    brand: 'Amazon',
+    category: 'Electronics',
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 7,
+  },
+  {
+    name: 'Choice Hair Dryer',
+    image: '/images/dryer.jpg',
+    description:
+      'Ionic Hair Dryer: This Styler features ionic technology with an On/Off switch, allowing you to control and customize your style; Turn ions on for smooth, shiny hair and off for more fullness and volume',
+    brand: 'Amazon',
+    category: 'Electronics',
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 7,
+  },
+  {
+    name: 'Human hair',
+    image: '/images/weave.jpg',
+    description:
+      'UNice Brown Highlight Body Wave Human Hair Weave 3 Bundles 16 18 20 inch, Brazilian Remy Hair Ombre Blonde Human Hair Wavy Weaves Sew in Piano Color TFB30',
+    brand: 'Amazon',
+    category: 'Electronics',
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 7,
+  },
+]
+
+export default products
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+
+const footer = () => {
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className='text-center py-3' >
+            Copyright &copy; TheShop
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
+
+export default footer
